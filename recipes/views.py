1c7d9e3bcc36b8d779ab246c33b5893229e9bce0
ItaloEng <franca.italo05@gmail.com>
2025-02-17 01:02:26 -0300

    height card

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Italo'})
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Italo'})
+    return render(request, 'recipes/pages/recipe-view.html', context={'resipe': make_recipe})
